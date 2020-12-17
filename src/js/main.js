d3a35c31235ee67e7ebfaad478c2ae38b8be0c77
jairoblatt <jairoblattc@gmail.com>
2020-12-17 00:28:05 -0300

    100 ligthhouse

--- src/js/main.js
+++ src/js/main.js
@@ -84,3 +84,16 @@
     });
   }
 })();
+// Scroll smooth
+const link = document.querySelector("#button-product");
+link.addEventListener("click", clickHandler);
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop + 50;
+  scroll({
+    top: offsetTop,
+    behavior: "smooth",
+  });
+}
